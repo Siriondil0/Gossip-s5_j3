a8b7c78526e15d4787b430b65bcfe496c166a2a4
ambre <elros.du.13@gmail.com>
2018-10-31 15:49:40 +0100

    ajout home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "welcome#index"
+  root "home#index"
   get '/contact', to: "contact#show"  
   get '/team', to: "team#index"
   resources :welcome
   resources :gossip
-
+  resources :home
 end
--- db/migrate/20181029094729_create_gossips.rb
+++ db/migrate/20181029094729_create_gossips.rb
@@ -1,6 +1,7 @@
 class CreateGossips < ActiveRecord::Migration[5.2]
   def change
     create_table :gossips do |t|
+      belongs_to :user, optional: true
       t.string :author
       t.text :content
       t.datetime :date
