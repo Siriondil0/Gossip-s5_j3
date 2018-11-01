07d669897fe6cffd6973e2601977efe5ea8c8e39
Fays <fay.meghzili@gmail.com>
2018-11-01 16:12:10 +0100

    modif user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
     def index
-      puts "formage"
-      puts session[:user_id]
         if session[:user_id]
           @current_user = User.find(session[:user_id])
         end
