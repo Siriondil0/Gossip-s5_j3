f0a7bbfc249d5e58e6080a3e790cd8d1d86a0ccd
guillaume <elros.du.13@gmail.com>
2018-11-01 17:10:05 +0100

    bon user comment

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,31 +1,36 @@
 class CommentController < ApplicationController
-    def new
-        @good_comment = Comment.new
-    end
+  def new
+    @good_comment = Comment.new
+  end
 
-    def create 
-        gossip = Gossip.find(params[:gossip_id])
-        comment = gossip.comments.create!(content: params[:comment][:content], user: gossip.user)
-        url="/gossip/" + gossip.id.to_s
-        redirect_to(url)
-    end
+  def create 
+    gossip = Gossip.find(params[:gossip_id])
+    if session[:user_id]
+      user = User.find(session[:user_id])
+    else
+      user = User.User.create!(city:City.first, first_name: "Ano", last_name: "Nimus", email: "ano.nimus@anomymus.com")
+    end  
+    comment = gossip.comments.create!(content: params[:comment][:content], user: user)
+    url="/gossip/" + gossip.id.to_s
+    redirect_to(url)
+  end
 
-    def edit
-        @good_comment = Comment.find(params[:id]) 
-    end
+  def edit
+    @good_comment = Comment.find(params[:id]) 
+  end
 
-    def update
-        @good_comment = Comment.find(params[:id]) 
-        @good_comment.update_attributes(content: params[:comment][:content])
-        url="/gossip/" + @good_comment.commentable.id.to_s
-        redirect_to(url)
-    end
+  def update
+    @good_comment = Comment.find(params[:id]) 
+    @good_comment.update_attributes(content: params[:comment][:content])
+    url="/gossip/" + @good_comment.commentable.id.to_s
+    redirect_to(url)
+  end
     
-    def destroy
-        @good_comment = Comment.find(params[:id])
-        url= @good_comment.commentable.id.to_s
-        @good_comment.destroy
-        redirect_to("/gossip/"+url)
+  def destroy
+    @good_comment = Comment.find(params[:id])
+    url= @good_comment.commentable.id.to_s
+    @good_comment.destroy
+    redirect_to("/gossip/"+url)
     end
 
 end
