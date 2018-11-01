fca9cf15c1c7150c739732b1a3d7e3df7982c2ea
Hai-Dang <haidang.hathuc@gmail.com>
2018-11-01 09:55:28 +0100

    render header

--- db/migrate/20181029094729_create_gossips.rb
+++ db/migrate/20181029094729_create_gossips.rb
@@ -1,7 +1,7 @@
 class CreateGossips < ActiveRecord::Migration[5.2]
   def change
     create_table :gossips do |t|
-      t.belongs_to :user, optional: true , index: true
+      t.belongs_to :user, index: true
       t.string :title
       t.text :content
       t.datetime :date
